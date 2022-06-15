68ca916beb0332ef485e68a12fd70ac5e6cc17d2
jcryptodev <jdl.lourens@gmail.com>
2022-06-15 19:56:59 +0200

    added intro html and js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const intro = document.querySelector(".intro");
+const logo = document.querySelector(".logo-header");
+const logoSpan = document.querySelectorAll(".logo");
+const loading = document.querySelector(".loading-container");
