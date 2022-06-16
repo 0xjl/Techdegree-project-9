a3867f21b3dd185b4020b4430202dc1cd93ee6d6
jcryptodev <jdl.lourens@gmail.com>
2022-06-16 21:05:34 +0200

    updated skills section

--- js/app.js
+++ js/app.js
@@ -4,11 +4,14 @@ const logoSpan = document.querySelectorAll(".logo");
 const loading = document.querySelector(".loading-container");
 let buttonToggle = document.getElementById("darkmode");
 let body = document.querySelector(".all");
-let card = document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");
 let nav = document.querySelector(".navbar-light");
 let navbg = document.querySelector(".bg-light");
 let svgICONS = document.querySelectorAll(".svg-icon");
 
+//card functions
+
+//dark mode functions
 function removeLight() {
   nav.classList.remove("navbar-light");
   navbg.classList.remove("bg-light");
@@ -33,7 +36,6 @@ function removeDark() {
   buttonToggle.classList.remove("btn-dark");
 }
 
-//functions
 function buttonChecker() {
   if (buttonToggle.innerText === "Dark") {
     buttonToggle.innerText = "Light";
@@ -52,8 +54,8 @@ function darkMode() {
   for (let i = 0; i < svgICONS.length; i++) {
     svgICONS[i].style.fill = "white";
   }
-  for (let i = 0; i < card.length; i++) {
-    card[i].style.backgroundColor = "black";
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].style.backgroundColor = "black";
   }
 }
 
@@ -65,11 +67,11 @@ function lightMode() {
   for (let i = 0; i < svgICONS.length; i++) {
     svgICONS[i].style.fill = "black";
   }
-  for (let i = 0; i < card.length; i++) {
-    card[i].style.backgroundColor = "white";
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].style.backgroundColor = "white";
   }
 }
-
+//intro functions
 function fadeText() {
   logoSpan.forEach((span, index) => {
     setTimeout(() => {
